a483b4eec9fb16b9f4a6e555a5b662f36f6927e9
Illusion <rohanthakur2684@gmail.com>
2026-01-10 21:16:43 +0530

    Update web.py

--- anvi/web.py
+++ anvi/web.py
@@ -1,1 +1,15 @@
+from flask import Flask, redirect
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return redirect("https://illusionoir.github.io/anvi", code=302)
+
+def run_web() -> None:
+    app.run(host="0.0.0.0", port=8080)
+
+def start_web() -> None:
+    Thread(target=run_web, daemon=True).start()
 
